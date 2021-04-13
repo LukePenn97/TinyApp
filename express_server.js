e7d474b767e40ba508ebb05d5c54aa991d572911
Luke Penner <lukepenner@hotmail.ca>
2021-04-12 20:06:49 -0700

    I almost cried tryin to implement this, now you can delete urls

--- express_server.js
+++ express_server.js
@@ -31,13 +31,24 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 app.get("/urls/:shortURL", (req, res) => {
-  console.log(req.params.shortURL);
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render("urls_show", templateVars);
+  //console.log(req.params.shortURL);
+  //const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const longURL = urlDatabase[req.params.shortURL];
+  if (longURL) {
+
+    res.redirect(longURL);
+  }
 });
 app.post("/urls", (req, res) => {
   //console.log(req.body);  // Log the POST request body to the console
-  res.send(generateRandomString());         // Respond with 'Ok' (we will replace this)
+  let randomString = generateRandomString();         // Respond with 'Ok' (we will replace this)
+  urlDatabase[randomString] = req.body.longURL;
+  res.redirect("/urls");
+});
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete(urlDatabase[req.params.shortURL]);
+  res.redirect("/urls");
 });
 
 app.get("/hello", (req, res) => {
